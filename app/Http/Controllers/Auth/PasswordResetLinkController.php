d9526659c1c5a4ce171adf39e124e08128024e51
Rizwan Azam <rizwanazam607@gmail.com>
2024-07-09 19:44:29 +0500

    Working on password reset

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -3,8 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Auth\Events\PasswordResetLinkSent;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Password;
 use Illuminate\Validation\ValidationException;
 
@@ -21,11 +23,18 @@ class PasswordResetLinkController extends Controller
             'email' => ['required', 'email'],
         ]);
 
+        $getToken = '';
+
         // We will send the password reset link to this user. Once we have attempted
         // to send the link, we will examine the response then see the message we
         // need to show to the user. Finally, we'll send out a proper response.
         $status = Password::sendResetLink(
-            $request->only('email')
+            $request->only('email'),
+            function($user, $token) use (&$getToken) {
+                $getToken = $token;
+                $user->sendPasswordResetNotification($token);
+                return Password::RESET_LINK_SENT;
+            }
         );
 
         if ($status != Password::RESET_LINK_SENT) {
@@ -34,6 +43,6 @@ class PasswordResetLinkController extends Controller
             ]);
         }
 
-        return response()->json(['status' => __($status)]);
+        return response()->json(['status' => __($status), 'token' => $getToken]);
     }
 }
