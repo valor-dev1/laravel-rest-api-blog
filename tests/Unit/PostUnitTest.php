469106eec0e8d4a305cb75c1bec8e17057eb47fd
Rizwan Azam <rizwanazam607@gmail.com>
2024-07-15 22:39:37 +0500

    updated class nme

--- tests/Unit/PostUnitTest.php
+++ tests/Unit/PostUnitTest.php
@@ -9,7 +9,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
-class PostTest extends TestCase
+class PostUnitTest extends TestCase
 {
     use RefreshDatabase, WithFaker;
 
